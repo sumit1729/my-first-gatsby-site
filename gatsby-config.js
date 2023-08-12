2a1c40062fb9bb65e7cffbcd38e5b4cc8850539e
sumit1729 <symit1729@outlook.com>
2023-08-12 17:07:12 +0600

    added new image to home

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `my-blog`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'll be making new changes as part of creating the blog</p>
+      <p>This is a blog to showcase our project and portfolios</p>
+      <StaticImage
+        alt="Sumit's profile picture"
+        src="../images/sumit.jpg"
+      />
     </Layout>
   )
 }
