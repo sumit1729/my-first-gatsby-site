19f8f4a22384b094f53f718f4165cb0b68dbdeea
sumit1729 <symit1729@outlook.com>
2023-08-12 17:45:05 +0600

    changed about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,8 +4,22 @@ import Layout from "../components/layout"
 // Step 2: Define your component
 const AboutPage = () => {
   return (
-    <Layout pageTitle="About Me">
-        <p>I am a proud creator of this site which I made using gatsby</p>
+    <Layout pageTitle="About Us">
+        <p>We are Making this site to showcase our skills</p>
+
+        <h3>BitBandits</h3>
+        <h4>we are a group of developers embarking on a journey</h4>
+
+        <div>
+          <h5>Group Members</h5>
+          <ul>
+            <li>Sumit chakraborty</li>
+            <li>Shakhawat Shihab</li>
+            <li>Pallob Poddar</li>
+            <li>Nahid Parvez</li>
+            <li>Mubashir Zahid</li>
+          </ul>
+        </div>
     </Layout>
   )
 }
