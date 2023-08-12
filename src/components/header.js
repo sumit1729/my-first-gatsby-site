8c242f193ad8b40383cf8b7b8e4cb82fb4e7e8e8
sumit1729 <symit1729@outlook.com>
2023-08-12 19:18:19 +0600

    added graphql queries

---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { useStaticQuery, graphql } from "gatsby";
+
+const Header = () => {
+    const data = useStaticQuery(graphql`
+    query {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+    }
+    `)
+
+    return (
+        <header>
+            <h1>{data.site.siteMetadata.title}</h1>
+        </header>
+    )
+}
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,15 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>This is a blog to showcase our project and portfolios</p>
+
+      {/* <Gallery/> */}
       <StaticImage
+        style={{ width: "300px", height: "300px" }}
         alt="Sumit's profile picture"
         src="../images/sumit.jpg"
       />
       <StaticImage
+        style={{ width: "300px", height: "300px" }}
         alt="Shakhawat's profile picture"
         src="../images/shakhawat.png"
       />
